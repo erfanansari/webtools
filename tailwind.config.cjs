66ca8b6f031e1394e5ca93c2f3eec68509be6034
Erfan Ansari <dev.erfanansari@gmail.com>
2023-02-12 18:23:53 +0330

    feat: add colors to tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,29 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          main: "#fb310a",
+          dark: "#981d05",
+        },
+        secondary: {
+          main: "#03032d",
+          light: "#1a1a3d",
+          dark: "#10091d",
+        },
+        neutral: {
+          main: "#a1a1c0",
+          light: "#f0f7ff",
+          // lighter: 'linear-gradient(to top, #f0f7ff 50%, #fff 120%)',
+          dark: "#6f6f86",
+        },
+        gray: {
+          1: "#eef6ff",
+          2: "#e6f0fe",
+        },
+      },
+    },
   },
   plugins: [],
 };
