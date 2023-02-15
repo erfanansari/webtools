e3e0417f066034d90c9c90f0046fa34ba8d9680d
Erfan Ansari <dev.erfanansari@gmail.com>
2023-02-16 00:41:04 +0330

    chore: use invalidateQuery instead of refetch in getting-started page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 import Head from "next/head";
-import Link from "next/link";
 
 import { useState } from "react";
 import { api } from "../utils/api";
@@ -11,9 +10,10 @@ const Home: NextPage = () => {
   const getAllQuery = api.example.getAll.useQuery();
   const toolsQuery = api.tool.getAll.useQuery();
   const getSecretMessageQuery = api.example.getSecretMessage.useQuery();
+  const context = api.useContext();
   const toolMutation = api.tool.create.useMutation({
     onSuccess: async () => {
-      await toolsQuery.refetch();
+      await context.tool.invalidate();
     },
   });
   const [toolName, setToolName] = useState("");
@@ -29,30 +29,6 @@ const Home: NextPage = () => {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
           </h1>
-          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
-          </div>
           <div className="flex flex-col items-center gap-2 text-white">
             <p className="text-2xl">
               {helloQuery.isLoading
@@ -117,7 +93,7 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
+export const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
