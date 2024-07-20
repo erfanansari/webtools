f4fdf4069705a9950a52ce7987da5a79493d04e8
Erfan Ansari <dev.erfanansari@gmail.com>
2024-07-20 14:12:26 +0330

    chore: remove enerything

--- prisma/migrations/20230216202621_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[url]` on the table `Tool` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Tool_url_key" ON "Tool"("url");
--- src/utils/helpers/formateDate.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(date: Date) {
-  return new Intl.DateTimeFormat("en-US", {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-    hour12: false,
-  }).format(new Date(date));
-}
